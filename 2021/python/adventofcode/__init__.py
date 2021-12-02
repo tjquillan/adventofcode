aca9418be444ec90c61f3f74f5af1a6343256abe
tjquillan <tjquillan@gmail.com>
2021-12-02 09:32:16 -0500

    Give get_input processing + add flake8 config

--- 2021/python/adventofcode/__init__.py
+++ 2021/python/adventofcode/__init__.py
@@ -1,10 +1,17 @@
 from pathlib import Path
+from typing import Callable, TypeVar
 
 __version__ = "2021"
 
+T = TypeVar("T")
 
-def get_input(day: int) -> list[int]:
+
+def _default_processor(val: str) -> str:
+    return val
+
+
+def get_input(day: int, processor: Callable[[str], T] = _default_processor) -> list[T]:
     path = Path.cwd().joinpath("..", "inputs", f"day{day}.txt")
 
     with path.open() as data_file:
-        return [int(n) for n in data_file.read().splitlines()]
+        return [processor(n) for n in data_file.read().splitlines()]
